e17ae0abc6965ce302817eae18412296df999583
eprince1 <eitanaprince@gmail.com>
2019-09-17 20:14:45 -0400

    change components

--- example/javascripts/components/StandardLayout.jsx
+++ example/javascripts/components/StandardLayout.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 export default class StandardLayout extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: ''
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ text: 'This is the standard layout'});
+    }
+
     render() {
         return (
-            <div>Standard Layout</div>
+            <div>
+                <h1>{this.state.text}</h1>
+            </div>
         );
     }
 }
