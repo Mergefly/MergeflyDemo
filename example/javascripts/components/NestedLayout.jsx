2998e8fe5313d4b7226233463c155288be479e48
eprince1 <eitanaprince@gmail.com>
2019-09-17 20:27:39 -0400

    addMoreFunctions

--- example/javascripts/components/NestedLayout.jsx
+++ example/javascripts/components/NestedLayout.jsx
@@ -68,26 +68,6 @@ export default function() {
               </pre>
               <Lorem title="3rd Pane" />
             </div>
-            <div className="my-pane">
-              <h2>4th Pane</h2>
-              <p>This is the 4th pane.</p>
-              <pre>
-                &lt;SplitterLayout primaryIndex={'{1}'} secondaryInitialSize={'{250}'}&gt;{'\n'}
-                &nbsp;&nbsp;&lt;div&gt;1st&lt;/div&gt;{'\n'}
-                &nbsp;&nbsp;&lt;SplitterLayout secondaryInitialSize={'{250}'}&gt;{'\n'}
-                &nbsp;&nbsp;&nbsp;&nbsp;&lt;SplitterLayout vertical secondaryInitialSize={'{250}'}&gt;{'\n'}
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div&gt;2nd&lt;/div&gt;{'\n'}
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;SplitterLayout secondaryInitialSize={'{250}'}&gt;{'\n'}
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div&gt;3rd&lt;/div&gt;{'\n'}
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>&lt;div&gt;4th&lt;/div&gt;</strong>{'\n'}
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/SplitterLayout&gt;{'\n'}
-                &nbsp;&nbsp;&nbsp;&nbsp;&lt;/SplitterLayout&gt;{'\n'}
-                &nbsp;&nbsp;&nbsp;&nbsp;&lt;div&gt;5th&lt;/div&gt;{'\n'}
-                &nbsp;&nbsp;&lt;/SplitterLayout&gt;{'\n'}
-                &lt;/SplitterLayout&gt;
-              </pre>
-              <Lorem title="4th Pane" />
-            </div>
           </SplitterLayout>
         </SplitterLayout>
         <div className="my-pane">
---
+++ example/javascripts/components/StandardLayout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class StandardLayout extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: ''
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ text: 'This is the standard layout'});
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.text}</h1>
+            </div>
+        );
+    }
+}
