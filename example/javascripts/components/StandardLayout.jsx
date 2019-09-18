16cdab0f4c3ceed4b6f0085840d28dcbdda2b548
eprince1 <eitanaprince@gmail.com>
2019-09-17 20:10:00 -0400

    add new component

---
+++ example/javascripts/components/StandardLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class StandardLayout extends React.Component {
+    render() {
+        return (
+            <div>Standard Layout</div>
+        );
+    }
+}
