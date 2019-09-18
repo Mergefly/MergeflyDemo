16100402108cef83ed6c6bb318960211f6ac1033
eprince1 <eitanaprince@gmail.com>
2019-09-17 20:38:51 -0400

    configure files

---
+++ src/components/Configure.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Configure extends React.Component {
+    render() {
+        return (
+            <div>Configuration File...</div>
+        );
+    }
+}
