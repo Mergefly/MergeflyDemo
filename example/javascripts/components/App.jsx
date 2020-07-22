d4f0d3ddc398326cb6958a6168b885f14b4e42da
Eitan Prince <eitanaprince@gmail.com>
2020-07-21 23:01:07 -0400

    initial updates

--- example/javascripts/components/App.jsx
+++ example/javascripts/components/App.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import NavLink from './NavLink';
+import React from "react";
+import PropTypes from "prop-types";
+import NavLink from "./NavLink";
 
 function constructLinks() {
   return (
     <ul className="navigation">
-      <li><NavLink to="/sidebar">Sidebar</NavLink></li>
-      <li><NavLink to="/events">Events</NavLink></li>
-      <li><NavLink to="/iframe">iframe</NavLink></li>
-      <li><NavLink to="/standard-horizontal">Horizontal</NavLink></li>
-      <li><NavLink to="/standard-vertical">Vertical</NavLink></li>
-      <li><NavLink to="/minimal-size">Pane Minimal Size</NavLink></li>
-      <li><NavLink to="/percentage">Width in Percentage</NavLink></li>
-      <li><NavLink to="/nested">Nested Layout</NavLink></li>
+      <li>
+        <NavLink to="/sidebar">Sidebar</NavLink>
+      </li>
+      <li>
+        <NavLink to="/events">Events</NavLink>
+      </li>
+      <li>
+        <NavLink to="/iframe">iframe</NavLink>
+      </li>
+      <li>
+        <NavLink to="/standard-horizontal">Standard Horizontal</NavLink>
+      </li>
+      <li>
+        <NavLink to="/standard-vertical">Standard Vertical</NavLink>
+      </li>
+      <li>
+        <NavLink to="/minimal-size">Pane Minimal Size</NavLink>
+      </li>
+      <li>
+        <NavLink to="/percentage">Width in Percentage</NavLink>
+      </li>
+      <li>
+        <NavLink to="/nested">Nested Layout</NavLink>
+      </li>
     </ul>
   );
 }
@@ -25,19 +41,15 @@ function App(props) {
         <p>A split layout for React and modern browsers.</p>
       </header>
       <div className="main">
-        <nav className="navigation-bar">
-          {constructLinks()}
-        </nav>
-        <div className="child-content">
-          {props.children}
-        </div>
+        <nav className="navigation-bar">{constructLinks()}</nav>
+        <div className="child-content">{props.children}</div>
       </div>
     </div>
   );
 }
 
 App.propTypes = {
-  children: PropTypes.element.isRequired
+  children: PropTypes.element.isRequired,
 };
 
 export default App;
--- example/javascripts/components/StandardLayout.jsx
+++ example/javascripts/components/StandardLayout.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 
 export default class StandardLayout extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            text: ''
-        };
-    }
+    this.state = {
+      text: "",
+    };
+  }
 
-    componentDidMount() {
-        this.setState({ text: 'This is the standard layout'});
-    }
+  componentDidMount() {
+    this.setState({ text: "This is the standard layout" });
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>{this.state.text}</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.text}</h1>
+      </div>
+    );
+  }
 }
